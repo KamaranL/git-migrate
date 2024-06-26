6b635415bfc0760909ef215692ac846edf07bbf6
Kamaran Layne <kamaranl@kamaranl.vip>
2024-06-26 15:04:28 -0400

    updated cd command to be evaluated

--- install.sh
+++ install.sh
@@ -92,7 +92,7 @@ cmd_dir="$dst_dir/commands"
     cry info Added commands to \$PATH. ||
     cry error Failed to add commands to \$PATH.
 
-cd "${dst_dir/\~/"$HOME"}"
+eval "cd $dst_dir"
 
 echo "'$src_dir'" can now be removed.
 
