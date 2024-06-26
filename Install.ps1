51790bded6b4714c3a47709e04f6abae3c3b67f9
Kamaran Layne <kamaranl@kamaranl.vip>
2024-06-26 12:19:03 -0400

    Added scripts

---
+++ Install.ps1
@@ -0,0 +1,118 @@
+[CmdletBinding()]
+[OutputType([void])]
+param ()
+
+# .DESCRIPTION
+# Installs Git config, hooks, and commands
+
+function Env {
+    [CmdletBinding()]
+    param (
+        # Variable name
+        [Parameter(
+            Mandatory,
+            Position = 0)]
+        [String]
+        $Key,
+
+        # Variable value
+        [Parameter(
+            Position = 1)]
+        [String]
+        $Value,
+
+        # Variable scope
+        [ValidateSet('User', 'Machine')]
+        [String]
+        $Scope
+    )
+
+    function Scope([string]$Scope) {
+        try {
+            $Result = [System.Environment]::GetEnvironmentVariable($Key, $Scope).TrimEnd($PS)
+
+            if ($Result -match $PS) {
+                $Result = $Result -split $PS
+            }
+
+            return $Result
+        } catch { return }
+    }
+
+    try {
+        if (!($Value)) {
+            if (!($Scope)) {
+                $User = Scope('User')
+                $Machine = Scope('Machine')
+
+                return $($User; $Machine)
+            } else {
+                return @(Scope($Scope))
+            }
+        } else {
+            Write-Verbose `
+                "Performing the operation `"Set environment variable`" on target `"Name: $Key Value: $Value`"."
+            [System.Environment]::SetEnvironmentVariable($Key, $Value, 'User')
+        }
+    } catch { return }
+}
+
+Set-StrictMode -Version Latest
+
+if ($PSBoundParameters.Verbose) { $VerbosePreference = 'Continue' }
+$DSC = [System.IO.Path]::DirectorySeparatorChar
+$PS = [System.IO.Path]::PathSeparator
+
+# check git version
+$MinimumVersion = 2.32
+$Version = (Get-Command 'git').Version
+if (($Version.Major, $Version.Minor -join '.') -lt $MinimumVersion) {
+    throw [System.NotSupportedException]::new("Minimum version of `"$MinimumVersion`" required")
+}
+
+# move current directory if not already moved
+$TargetDir = $HOME, '.config', 'git' -join $DSC
+$CurrentDir = (Get-Item $PSCommandPath).DirectoryName
+Write-Verbose 'Checking gitconfig...'
+if ($CurrentDir -ne $TargetDir) {
+    $CopyItems = 'Copy-Item $CurrentDir\* $TargetDir -Recurse -Force'
+
+    if (Test-Path $TargetDir -PathType Container) {
+        $CopyItems += ' -Confirm'
+    } else {
+        Write-Verbose "Performing the operation `"Create Directory`" on target `"Destination: $TargetDir`""
+        $null = New-Item $TargetDir -ItemType Directory -Force
+    }
+
+    if ($PSBoundParameters.Verbose) { $CopyItems += ' -Verbose' }
+
+    Invoke-Command -ScriptBlock ([ScriptBlock]::Create($CopyItems))
+}
+
+# backup existing global config
+$OldGlobalConfig = $HOME, '.gitconfig' -join $DSC
+if (Test-Path $OldGlobalConfig -PathType Leaf) {
+    Copy-Item $OldGlobalConfig "$OldGlobalConfig.bak" -Force -Confirm:$false
+    if ($?) { Remove-Item $OldGlobalConfig -Force -Confirm:$false }
+}
+
+# new global config
+$GlobalConfig = $TargetDir, 'conf', 'main.conf' -join $DSC
+
+# set env vars
+if ((Env 'GIT_CONFIG_NOSYSTEM') -notcontains 'true') { Env 'GIT_CONFIG_NOSYSTEM' 'true' }
+if ((Env 'GIT_CONFIG_GLOBAL') -notcontains $GlobalConfig) { Env 'GIT_CONFIG_GLOBAL' $GlobalConfig }
+
+# add commands to PATH
+$CommandsDir = $TargetDir, 'commands' -join $DSC
+if ((Env 'Path') -notcontains $CommandsDir) {
+    $Path = "$CommandsDir$PS"
+    $Path += (Env 'Path' -Scope User) -join $PS
+    Env 'Path' $Path
+}
+
+'Done!'
+
+Set-Location $TargetDir
+
+exit 0
---
+++ install.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+## install.sh: Installs Git config, hooks, and commands
+
+cry()
+{
+    [ "${1,,}" == error ] &&
+        echo "${1^}: ${@:2}" 1>&2 ||
+        echo "${1^}: ${@:2}"
+    logger -p local0."$1" -t git-migrate "${@:2}"
+
+    return 0
+}
+
+try()
+{
+    if "$@"; then
+        cry debug "$@"
+        return 0
+    else
+        cry error Failed to execute "'$@'"
+    fi
+}
+
+die()
+{
+    cry error "$@"
+    exit 1
+}
+
+# check dependencies
+deps=(
+    awk
+    bc
+    cp
+    find
+    git
+    realpath
+    xargs
+)
+
+for dep in "${deps[@]}"; do
+    ! which "$dep" >/dev/null 2>&1 &&
+        die Missing dependency "'$dep'".
+done
+
+# check git version
+min_ver=2.32
+[[ $(git --version) =~ [0-9]+\.[0-9]+ ]] &&
+    git_ver=$BASH_REMATCH
+cry info Identified git version as "'$git_ver'".
+
+((!$(bc <<<"$git_ver > $min_ver"))) &&
+    die Minimum version of "'$min_ver'" required.
+
+# move current directory if not already moved
+dst_dir="$HOME/.config/git2"
+src_dir="$(realpath .)"
+[ "$src_dir" != "$dst_dir" ] && {
+    [ ! -d "$dst_dir" ] && try mkdir -p "$dst_dir"
+
+    try cp -Rfp "$src_dir"/ "$dst_dir"
+}
+dst_dir="${dst_dir/"$HOME"/\~}"
+
+# backup existing global config
+old_gcfg="$HOME/.gitconfig"
+[ -f "$old_gcfg" ] &&
+    try cp -fp "$old_gcfg" "$old_gcfg.bak" &&
+    rm -rf "$old_gcfg"
+
+# new global config
+gcfg="$dst_dir/conf/main.conf"
+
+# set env vars
+[ "$(env | awk -F= '/^GIT_CONFIG_GLOBAL=.*$/ {print $2}')" != "$gcfg" ] &&
+    echo export GIT_CONFIG_GLOBAL="$gcfg" >>"$HOME/.bash_profile"
+((!$?)) &&
+    cry info Set environment variable "'GIT_CONFIG_GLOBAL=$gcfg'". ||
+    cry error Failed to set GIT_CONFIG_GLOBAL.
+[ "$(env | awk -F= '/^GIT_CONFIG_NOSYSTEM=.*$/ {print $2}')" != "true" ] &&
+    echo export GIT_CONFIG_NOSYSTEM=true >>"$HOME/.bash_profile"
+((!$?)) &&
+    cry info Set environment variable "'GIT_CONFIG_NOSYSTEM=$gcfg'". ||
+    cry error Failed to set GIT_CONFIG_NOSYSTEM.
+
+# add commands to PATH
+cmd_dir="$dst_dir/commands"
+[[ ! $PATH =~ $cmd_dir ]] &&
+    echo export PATH="$cmd_dir:\"\$PATH\"" >>"$HOME/.bash_profile"
+((!$?)) &&
+    cry info Added commands to \$PATH. ||
+    cry error Failed to add commands to \$PATH.
+
+echo Done!
+
+cd "$dst_dir"
+
+exit 0
